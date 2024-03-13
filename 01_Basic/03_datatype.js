52a324e235b36296083788960b130b43092b463e
shahrukh873 <113853744+shahrukh873@users.noreply.github.com>
2024-03-13 17:35:25 +0000

    Opeartion

---
+++ 01_Basic/03_datatype.js
@@ -0,0 +1,17 @@
+"use strict"  // treat all code as new version
+
+//alert(3+4) // we are using node not Browser
+
+ 
+let name = "shahrukh" // Javascript automatically fetch the datatype depent on how you wright the code
+
+let age=  22
+
+let isLogin = true
+
+console.log(typeof name)
+
+console.log(typeof isLogin)
+
+console.log(typeof age)
+    
---
+++ 01_Basic/04_conversion.js
@@ -0,0 +1,45 @@
+// let score  = "2abcx"
+
+// console.log(typeof score);
+
+// let scoreInNumber = Number(score)
+
+// console.log(typeof scoreInNumber);
+
+// console.log(score)
+
+// console.log(scoreInNumber)
+
+// // 
+
+// "22"  =22
+// "22abc"  = NaN
+
+
+//if we want to convert into string
+
+// let scorev = 1
+
+// let scorevarible = String(scorev);
+
+// console.log(typeof scorevarible)
+// console.log(scorevarible) ;
+
+
+/*
+
+****************************Operation**********************************
+
+*/
+
+
+let value = 3
+
+let negvalue = -value
+
+console.log(negvalue)
+
+console.log(5%3);
+
+console.log("2"+12); // if string come first output will be string in its self output = 212
+
