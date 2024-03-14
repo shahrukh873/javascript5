e9daa28e033c2fac04dace53f2b854d1b9a63893
shahrukh873 <113853744+shahrukh873@users.noreply.github.com>
2024-03-14 18:07:18 +0000

    datatype

---
+++ 01_Basic/05_datatype.js
@@ -0,0 +1,28 @@
+// // Primitive and non primitive thier are two data type 
+
+// const Id = Symbol('123')
+
+// const diffrentId = Symbol('123')
+
+// console.log(Id);
+
+// console.log(diffrentId);
+
+// console.log(Id==diffrentId); // This is not equal beause symbol stored the two diffrent value
+
+const  nabi = ["Mohammad" , "musa", "aadm"] ;
+
+// Object are defind in the {} bracket or we can defind this oin variable in {}bracket .
+
+let letObject =
+{
+     name : "Shahrukh",
+     age : 22 , 
+
+}
+
+let myFunction = 
+ function()
+ {
+    console.log("Shahrukh")
+ }
