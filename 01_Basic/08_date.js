463e7a4ab1d5dbcb04bf1733adcd8fe0df13b5c1
shahrukh873 <113853744+shahrukh873@users.noreply.github.com>
2024-04-06 18:14:46 +0000

    date

---
+++ 01_Basic/08_date.js
@@ -0,0 +1,26 @@
+let myDate = new Date();
+
+// This is not is understable format
+// console.log(myDate);
+
+// So we convet it into string 
+
+// console.log(myDate.toString());
+
+// Convert into date
+
+// let myNewDate = new Date(2024,03,06) ;
+
+// let myNewDate2 = new Date("04-13-2024") ;
+// // Month start with 0 ;
+// // console.log(myNewDate.toDateString());
+
+// console.log(myNewDate2.toLocalString());
+
+
+let myTime = Date.now();
+
+console.log(myDate);
+
+
+
